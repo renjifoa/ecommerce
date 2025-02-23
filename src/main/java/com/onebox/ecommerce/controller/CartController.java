93ca94397b1db2b4212446635de4b76d692ecdfc
renjifoa <adrianarenjifo@gmail.com>
2025-02-23 21:42:20 +0100

    Added Swager Tags and update controllerAdvice

--- src/main/java/com/onebox/ecommerce/controller/CartController.java
+++ src/main/java/com/onebox/ecommerce/controller/CartController.java
@@ -3,6 +3,11 @@ package com.onebox.ecommerce.controller;
 import com.onebox.ecommerce.model.Cart;
 import com.onebox.ecommerce.model.Product;
 import com.onebox.ecommerce.service.CartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -29,25 +34,57 @@ public class CartController {
         this.cartService = cartService;
     }
 
+    @Operation(summary = "Create a new cart")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Cart created",
+                    content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Cart.class)) })
+    })
     @PostMapping
     public ResponseEntity<Cart> createCart() {
         Cart cart = cartService.createCart();
         return new ResponseEntity<>(cart, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Get a cart by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cart found",
+                    content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Cart.class)) }),
+           @ApiResponse(responseCode = "404", description = "Cart not found",
+                    content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = WebRestControllerAdvice.ErrorResponse.class)) })
+    })
     @GetMapping("/{cartId}")
     public ResponseEntity<Cart> getCartById(@PathVariable Long cartId) {
         Cart cart =  cartService.getCartById(cartId);
         return new ResponseEntity<>(cart, HttpStatus.OK);
     }
 
+    @Operation(summary = "Update products in a cart")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cart updated",
+                    content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Cart.class)) }),
+            @ApiResponse(responseCode = "404", description = "Cart not found",
+                    content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = WebRestControllerAdvice.ErrorResponse.class)) })
+    })
     @PutMapping("/{cartId}")
     public ResponseEntity<Cart> updateProductsFromCart(@PathVariable Long cartId,
-                                                   @Valid @RequestBody List<Product> product) {
-        Cart cart =  cartService.updateProductsFromCart(cartId, product);
+                                                       @Valid @RequestBody List<Product> products) {
+        Cart cart =  cartService.updateProductsFromCart(cartId, products);
         return new ResponseEntity<>(cart, HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete a cart by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Cart deleted",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Cart not found",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = WebRestControllerAdvice.ErrorResponse.class)) })
+    })
     @DeleteMapping("/{cartId}")
     public ResponseEntity<Cart> deleteCart(@PathVariable Long cartId) {
         cartService.deleteCart(cartId);
--- src/main/java/com/onebox/ecommerce/controller/WebRestControllerAdvice.java
+++ src/main/java/com/onebox/ecommerce/controller/WebRestControllerAdvice.java
@@ -1,19 +1,28 @@
 package com.onebox.ecommerce.controller;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.Map;
 
 @RestControllerAdvice
 public class WebRestControllerAdvice {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Map<String, String> handleIllegalArgumentException(IllegalArgumentException ex) {
-        return Map.of("error", ex.getMessage());
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        ErrorResponse errorResponse = new ErrorResponse( ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
+    @Getter
+    @Setter
+    public static class ErrorResponse {
+        private String error;
+
+        public ErrorResponse(String error) {
+            this.error = error;
+        }
+    }
 }
