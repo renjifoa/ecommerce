bbc1398df5ae180d34e365ab08e0fd2c4f40dac1
renjifoa <adrianarenjifo@gmail.com>
2025-02-23 19:20:40 +0100

    Change type variable

--- src/main/java/com/onebox/ecommerce/model/Product.java
+++ src/main/java/com/onebox/ecommerce/model/Product.java
@@ -1,14 +1,17 @@
 package com.onebox.ecommerce.model;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class Product {
+    @NotNull
     private final Long id;
     private final String description;
-    private final int amount;
+    @NotNull
+    private final Integer amount;
 
-    public Product(Long id, String description, int amount) {
+    public Product(Long id, String description, Integer amount) {
         this.id = id;
         this.description = description;
         this.amount = amount;
